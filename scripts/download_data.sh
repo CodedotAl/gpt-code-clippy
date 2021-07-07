c7d3719bf464fef69374c2d866410065e126ae9e
ncoop57 <nacooper01@email.wm.edu>
2021-07-07 18:57:07 +0000

    Update data scripts for downloading stuff without OOMing

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
-COMBINED_REPOS=$1
+REPO_SHARDS=$1
 OUT_DIR=$2
-python convert_to_gh_downloader_format.py $COMBINED_REPOS $OUT_DIR
+ID=0
+for shard in ${REPO_SHARDS}/*; do
+    echo "Processing data from ${shard}"
+    python convert_to_gh_downloader_format.py $shard $OUT_DIR
+    ID=$(($ID + 1))
+    cd $OUT_DIR
+    python download_repo_text.py
+    mv $OUT_DIR"github_data" $OUT_DIR"github_data_${ID}"
+    cd -
+    echo "Finished processsing data from ${shard}"
+done
+# python convert_to_gh_downloader_format.py $COMBINED_REPOS $OUT_DIR
 
-cd $OUT_DIR
-python download_repo_text.py
-cd -
+# cd $OUT_DIR
+# python download_repo_text.py
+# cd -
