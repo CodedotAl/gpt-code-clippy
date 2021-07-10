5ce84e2bcc0739e3a9a3f5931e90a9ec4a7c15bd
reshinthadithyan <reshinth.adith@gmail.com>
2021-07-10 23:41:07 +0530

    adding parse score to evaluator

--- metrics/extrinsic_eval.py
+++ metrics/extrinsic_eval.py
@@ -1,6 +1,9 @@
 from metrics.bleu import compute_bleu
+from metrics.parse_check import check_parse
 
-def compute_metrics(references,generated) -> dict:
+Parser = check_parse() #Initializing parser
+
+def compute_metrics(references,generated,lang) -> dict:
     """
     Calculates various metrics and returns the calculated dict of these matrics.
     args:
@@ -8,11 +11,12 @@ def compute_metrics(references,generated) -> dict:
           reference should be tokenized into a list of tokens.
         translation: list of translations to score. Each translation
           should be tokenized into a list of tokens.
+        lang(str) : The language generated code belongs to
     returns:
         A dicitonary with different metrics intact.
     """
     metrics_dict = {} #Update as in new metrics are added over here.
     metrics_dict["smoothed_bleu_4"] = compute_bleu(references,generated,smooth=True)
     metrics_dict["bleu_4"] = compute_bleu(references,generated,smooth=False)
-    
+    metrics_dict["parse_score"] = Parser(generated,lang)["parse_score"]
     return metrics_dict
