2feb64b502e6e839246b0b6f12225558b44bc52a
arampacha <aruthart@gmail.com>
2021-07-10 00:27:01 +0000

    script v2 w setreaming eval ds

--- run_clm_streaming.sh
+++ run_clm_streaming.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-./run_clm_streaming_flax.py \
+./run_clm_streaming_flax_v2.py \
     --output_dir $HOME/gpt-neo-125M-code-clippy \
     --model_name_or_path="EleutherAI/gpt-neo-125M" \
     --dataset_name $HOME/gpt-code-clippy/code_clippy.py \
-    --data_dir $HOME/data \
+    --data_dir /home/shared/code-clippy-dataset/merged-data \
     --text_column_name="text" \
     --do_train \
     --block_size="1024" \
@@ -17,18 +17,19 @@
     --adam_beta2="0.98" \
     --weight_decay="0.01" \
     --overwrite_output_dir \
-    --max_steps 1000 \
+    --max_steps 30000 \
     --num_train_epochs="1" \
-    --logging_steps="100" \
-    --eval_steps="200" \
+    --logging_steps="50" \
+    --eval_steps="500" \
     --push_to_hub="False" \
     --report_to="all" \
     --dtype="bfloat16" \
     --skip_memory_metrics="False" \
-    --save_steps="200" \
+    --save_steps="500" \
     --save_total_limit 2 \
-    --gradient_accumulation_steps 1 \
-    --report_to="none" \
+    --gradient_accumulation_steps 4 \
+    --report_to="wandb" \
+    --max_eval_samples="5000" \
     # --resume_from_checkpoint $HOME/gpt-neo-125M-code-clippy/ckpt_201 \
     # --max_train_samples="10000" \
-    # --max_eval_samples="1000"
+    
