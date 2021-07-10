240816c55e698a923b146ba4af9200c2fc532761
arampacha <69807323+arampacha@users.noreply.github.com>
2021-07-10 17:52:50 +0300

    GPTNeo hypers (#35)
    * store full train state
    
    * close dl processes when done
    
    * gpt3 scheduler

--- run_clm_streaming.sh
+++ run_clm_streaming.sh
@@ -6,30 +6,32 @@
     --data_dir /home/shared/code-clippy-dataset/merged-data \
     --text_column_name="text" \
     --do_train --do_eval \
-    --block_size="1024" \
-    --per_device_train_batch_size="16" \
+    --block_size="2048" \
+    --per_device_train_batch_size="8" \
     --per_device_eval_batch_size="16" \
     --preprocessing_num_workers="8" \
-    --learning_rate="3e-4" \
+    --learning_rate="6e-4" \
     --adafactor \
-    --warmup_steps="250" \
+    --max_steps 10000 \
+    --warmup_steps 3000 \
+    --decay_steps 5000 \
     --adam_beta1="0.9" \
-    --adam_beta2="0.98" \
+    --adam_beta2="0.95" \
     --weight_decay="0.01" \
     --overwrite_output_dir \
-    --max_steps 50000 \
     --num_train_epochs="1" \
     --logging_steps="50" \
-    --eval_steps="500" \
+    --eval_steps="50" \
     --push_to_hub="False" \
     --report_to="all" \
     --dtype="bfloat16" \
     --skip_memory_metrics="False" \
-    --save_steps="500" \
+    --save_steps="50" \
     --save_total_limit 2 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 8 \
     --report_to="wandb" \
     --max_eval_samples="1000" \
+    --save_optimizer true \
     # --resume_from_checkpoint $HOME/gpt-neo-125M-code-clippy/ckpt_201 \
     # --max_train_samples="10000" \
     
