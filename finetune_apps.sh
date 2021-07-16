ec042fd0fbeb33fbdd404383a96001c609cfd79d
arampacha <aruthart@gmail.com>
2021-07-16 19:23:52 +0000

    upd apps fine-tuning script

--- finetune_apps.sh
+++ finetune_apps.sh
@@ -1,29 +1,29 @@
 #! /bin/bash
 ./run_clm_apps.py \
-    --output_dir $HOME/gpt-code-clippy-apps-2 \
-    --model_name_or_path EleutherAI/gpt-neo-1.3B \
+    --output_dir $HOME/gpt-code-clippy-apps-125m-2048-raw \
+    --model_name_or_path $HOME/gpt-code-clippy-125M-bs2048-raw \
     --dataset_name ./apps.py \
     --do_train --do_eval \
     --block_size="1024" \
-    --per_device_train_batch_size="2" \
-    --per_device_eval_batch_size="2" \
+    --per_device_train_batch_size="16" \
+    --per_device_eval_batch_size="16" \
     --preprocessing_num_workers="16" \
-    --learning_rate="2e-5" \
-    --warmup_steps="5000" \
+    --learning_rate="5e-5" \
+    --warmup_steps="800" \
     --adam_beta1="0.9" \
     --adam_beta2="0.98" \
     --weight_decay="0.1" \
     --overwrite_output_dir \
     --num_train_epochs="5" \
     --logging_steps="20" \
-    --eval_steps="1000" \
+    --eval_steps="100" \
     --push_to_hub="False" \
     --report_to="wandb" \
     --dtype="bfloat16" \
     --skip_memory_metrics="False" \
-    --save_steps="1000" \
+    --save_steps="100" \
     --save_strategy epoch \
-    --save_total_limit 2 \
+    --save_total_limit 5 \
     --gradient_accumulation_steps 2 \
     --adafactor \
     # --resume_from_checkpoint $HOME/gpt-neo-125M-code-clippy/ckpt_201 \
