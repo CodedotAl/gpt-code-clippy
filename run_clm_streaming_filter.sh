92e00c68552444e75728f32fef702423595bc561
arampacha <69807323+arampacha@users.noreply.github.com>
2021-07-17 03:07:39 +0300

    Dataloaders (#55)
    * adds online data filtering
    
    * Add new lang extensions to filter by
    
    * Switch script to use filtering
    
    * Add C++ headers also `lang_exts` should be a set
    
    * script for filtered run
    
    Co-authored-by: ncoop57 <nacooper01@email.wm.edu>
    Co-authored-by: Santiago Hincapie <shpotes3@gmail.com>

--- run_clm_streaming_filter.sh
+++ run_clm_streaming_filter.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-./run_clm_streaming_filter_flax_.py \
-    --output_dir $HOME/gpt-neo-125M-code-clippy-dedup-filtered-no-resize-2048bs \
-    --model_name_or_path="EleutherAI/gpt-neo-125M" \
+./run_clm_streaming_filter_flax.py \
+    --output_dir $HOME/gpt-code-clippy-lr1e-4-bs1024-f \
+    --model_name_or_path EleutherAI/gpt-neo-125M \
     --dataset_name $HOME/gpt-code-clippy/code_clippy_filter.py \
     --data_dir /home/shared/code_clippy_dedup_data \
     --text_column_name="text" \
@@ -12,23 +12,23 @@
     --preprocessing_num_workers="8" \
     --learning_rate="1e-4" \
     --max_steps 100000 \
-    --warmup_steps 2000 \
-    --decay_steps 30000 \
+    --warmup_steps 4000 \
+    --decay_steps 50000 \
     --adam_beta1="0.9" \
     --adam_beta2="0.95" \
     --weight_decay="0.1" \
     --overwrite_output_dir \
-    --logging_steps="25" \
-    --eval_steps="500" \
-    --push_to_hub="False" \
+    --logging_steps="50" \
+    --eval_steps="1000" \
+    --push_to_hub="True" \
     --report_to="all" \
     --dtype="bfloat16" \
-    --skip_memory_metrics="True" \
-    --save_steps="500" \
-    --save_total_limit 10 \
-    --gradient_accumulation_steps 32 \
+    --skip_memory_metrics="False" \
+    --save_steps="1000" \
+    --save_total_limit 5 \
+    --gradient_accumulation_steps 16 \
     --report_to="wandb" \
-    --run_name="gpt-neo-125M-code-clippy-dedup-filtered-no-resize-2048bs" \
+    --run_name="testing" \
     --max_eval_samples 2000 \
     --save_optimizer true \
     # --adafactor \
--- utils.py
+++ utils.py
@@ -136,3 +136,68 @@ class PrefetchDataloader(multiprocessing.Process):
     
     def __iter__(self):
         return self
+
+
+class PrefetchDataloaderWithFilter(multiprocessing.Process):
+    "Prefetch dataloader for IterableDataset"
+    def __init__(
+        self, dataset, max_steps, batch_size, sequence_length, prefetch_buffer=1, shuffle=True, shuffle_buffer=1000, seed=0,
+
+    ):
+        super().__init__(daemon=True)
+        self.max_steps = max_steps
+        self.bs = batch_size
+        self.seq_len = sequence_length
+        self.max_length = batch_size * sequence_length
+        self.prefetch_buffer = prefetch_buffer
+        self.shuffle = shuffle
+        self.shuffle_buffer = shuffle_buffer
+        self.seed = seed
+        self.dataset = dataset
+        self.make_iter()
+        self.queue = multiprocessing.Queue(prefetch_buffer)
+        self.rem = defaultdict(list)
+        self.start()
+
+    def make_iter(self):
+        if self.shuffle:
+            shuffled_dataset = self.dataset.shuffle(self.shuffle_buffer, seed=self.seed)
+            self.seed += 1
+            self.ds_iter = iter(shuffled_dataset)
+        else:
+            self.ds_iter = iter(self.dataset)
+
+    def __next__(self):
+        return make_batch(self.queue.get())
+        
+    def run(self):
+        i = 0
+        while True and i < self.max_steps:
+            # prepair next batch
+            sample = self.rem.copy()
+            l = len(sample["input_ids"])
+            max_length = self.max_length
+            while l < max_length:
+                try:
+                    next_sample = next(self.ds_iter)
+                except StopIteration:
+                    # reset generator if a pass through dataset is completed
+                    self.make_iter()
+                    next_sample = next(self.ds_iter)
+
+                sample_len = len(next_sample["input_ids"])
+                if sample_len < 200 or sample_len > 200_000:
+                    continue
+                l += sample_len
+                sample = {k:sample[k]+next_sample[k] for k in next_sample.keys()}
+            
+            self.rem = {k:v[max_length:] for k,v in sample.items()}
+            sample = {k:v[:max_length] for k,v in sample.items()}
+            # regroup to shape [bs x seq_len]
+            samples = {k:np.array([v[i*self.seq_len:(i+1)*self.seq_len] for i in range(self.bs)]) for k,v in sample.items()}
+            
+            self.queue.put(samples)
+        self.queue.put(None)
+    
+    def __iter__(self):
+        return self
