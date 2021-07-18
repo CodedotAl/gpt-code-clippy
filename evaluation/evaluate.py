72c149593e73a86cf6b2e2d0b46dbb8e4be64f6e
ncoop57 <nacooper01@email.wm.edu>
2021-07-18 09:29:54 -0400

    Update eval to use pytorch (it is faster and has more generation options)

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -17,31 +17,49 @@ from subprocess import check_output
 from transformers import (
     AutoTokenizer,
     FlaxGPTNeoForCausalLM,
+    GPTNeoForCausalLM
 )
 
-bleu = load_metric("sacrebleu")
+# bleu = load_metric("sacrebleu")
 
-MAX_TOKENs = 1024
+MAX_TOKS = 1024
+MAX_NEW_TOKS = 256
 model_name_or_path = "flax-community/gpt-code-clippy-125M-1024-f" # "flax-community/gpt-code-clippy-125M-bs2048-raw" # "EleutherAI/gpt-neo-125M"
 branch = "main"
 
 tokenizer = AutoTokenizer.from_pretrained(
     model_name_or_path, padding_side="left", pad_token="<|endoftext|>"
 )
-model = FlaxGPTNeoForCausalLM.from_pretrained(
+# model = FlaxGPTNeoForCausalLM.from_pretrained(
+#     model_name_or_path,
+#     pad_token_id=50256,
+#     revision=branch
+# )
+model = GPTNeoForCausalLM.from_pretrained(
     model_name_or_path,
     pad_token_id=50256,
     revision=branch
-)
+).to("cuda")
 
 
-def generate_text(prompt):
-    inputs = tokenizer(prompt, return_tensors="jax")  # .to("cuda")
-    output_seq = model.generate(input_ids=inputs.input_ids, max_length=MAX_TOKENs, early_stopping=True)
-    output = tokenizer.decode(output_seq["sequences"][0])#, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+def generate_text_jax(prompt):
+    inputs = tokenizer(prompt, return_tensors="jax")
+    output_seq = model.generate(
+        input_ids=inputs.input_ids, do_sample=True, max_length=MAX_TOKS, temperature=0.8
+    )
+    output = tokenizer.decode(output_seq["sequences"][0])
     # print(output)
     return output
 
+def generate_text(prompt, n):
+    inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
+    output_seq = model.generate(
+        input_ids=inputs.input_ids, max_length=MAX_TOKS,
+        max_new_tokens=MAX_NEW_TOKS, 
+        do_sample=True, temperature=0.8,
+        num_return_sequences=n
+    )
+    return tokenizer.batch_decode(output_seq, skip_special_tokens=False)
 
 def _eval_concode(path):
     # TODO: format input to model same as App and OpenAI HumanEval datasets are formatted
@@ -67,77 +85,24 @@ def _eval_apps(path):
         json.dump(gpt_codes, f)
 
     eval_and_save_problems(path, path.parent)
-        # prompt = generate_prompt(
-        #     Args(),
-        #     test_case_path,
-        #     prompt_path,
-        #     solutions_path,
-        #     tokenizer,
-        #     starter_path=starter_path,
-        # )
 
 
-
-
-
-    prob_paths = sorted(path.glob("*/"))
-    # map prob_paths to strings and save as a json file
-    str_paths = [str(p) for p in prob_paths]
-    with open(path / "test.json", "w") as f:
-        json.dump(str_paths, f)
-    for index, prob_path in enumerate(prob_paths[:2]):
-        test_case_path = prob_path / "input_output.json"
-        prompt_path = prob_path / "question.txt"
-        starter_path = prob_path / "starter_code.py"
-        solutions_path = prob_path / "solutions.json"
-        if not starter_path.exists():
-            starter_path = None
-        if not test_case_path.exists() or not prompt_path.exists():
-            continue
-        prompt = generate_prompt(
-            Args(),
-            test_case_path,
-            prompt_path,
-            solutions_path,
-            tokenizer,
-            starter_path=starter_path,
-        )
-        output = generate_text(prompt)
-        print(output)
-        # print(output)
-        gpt_codes[index] = output
-        # print(output)
-
-    with open(path.parent / "all_codes.json", "w") as f:
-        json.dump(gpt_codes, f)
-
-    eval_and_save_problems(path, path.parent)
-
-    # execute bash command to run eval script
-    # results = check_output(
-    #     [
-    #         # python3 test_one_solution.py -t /path/to/apps/test --save /path/to/save_dir --print_results
-    #         "python",
-    #         "./apps_utils/test_one_solution.py",
-    #         "-t",
-    #         str(path),
-    #         "--save",
-    #         str(path.parent),
-    #         "--print_results",
-    #     ]
-    # ).decode("utf-8")
-
-
-#     test_case_path = os.path.join(prob_path, "input_output.json")
-#     prompt_path = os.path.join(prob_path, "question.txt")
-#     starter_path = os.path.join(prob_path, "starter_code.py")
-#     solutions_path = os.path.join(prob_path, "solutions.json")
-#  generate_prompt(args, test_case_path, prompt_path, solutions_path, tokenizer, starter_path=None)
-
-
-def _eval_human_eval(path):
-    # problems = read_problems(str(path))
-    # num_samples_per_task = 1
+def _eval_human_eval(path, out_path):
+    problems = read_problems(str(path))
+    num_samples_per_task = 2
+    samples = []
+    for task_id in tqdm(list(problems.keys())):
+        for text in generate_text(problems[task_id]["prompt"], num_samples_per_task):
+            samples.append(dict(task_id=task_id, completion=text))
+    # samples = [
+    #     dict(
+    #         task_id=task_id,
+    #         completion=,
+    #     )
+    #     for text in generate_text(problems[task_id]["prompt"], num_samples_per_task)
+    #     for task_id in tqdm(list(problems.keys()))
+    #     for _ in range(num_samples_per_task)
+    # ]
     # samples = [
     #     dict(
     #         task_id=task_id,
@@ -146,17 +111,11 @@ def _eval_human_eval(path):
     #     for task_id in tqdm(list(problems.keys()))
     #     for _ in range(num_samples_per_task)
     # ]
-    # write_jsonl("human_eval.jsonl", samples)
-    # execute bash command to run eval script
-    results = evaluate_functional_correctness("human_eval.jsonl", [1], 4, 3.0, str(path))
-    # results = check_output(
-    #     [
-    #         "python",
-    #         path / "evaluate_functional_correctness.py",
-    #         "human_eval.jsonl",
-    #     ]
-    # ).decode("utf-8")
+    
+    write_jsonl(str(out_path / "human_eval.jsonl"), samples)
 
+    # execute bash command to run eval script
+    results = evaluate_functional_correctness(str(out_path / "human_eval.jsonl"), [1, 2], 4, 3.0, str(path))
     print(results)
 
 
@@ -165,12 +124,18 @@ def main(
     concode_path: Param("Path to the concode data in CodeXGLUE", str),
     apps_path: Param("Path to the the App dataset", str),
     human_eval_path: Param("Path to the human eval dataset", str),
+    out_path: Param("Path to save results", str),
 ):
     concode_path = Path(concode_path)
     apps_path = Path(apps_path)
     human_eval_path = Path(human_eval_path)
+    out_path = Path(out_path)
+    out_path = out_path / model_name_or_path.split("/")[-1]
+    out_path.mkdir(exist_ok=True)
+
+
     # _eval_concode(concode_path)
-    _eval_human_eval(human_eval_path)
+    _eval_human_eval(human_eval_path, out_path)
     # _eval_apps(apps_path)
     # dataset = load_dataset("json", data_files=str(concode_path / "test.json"))
     # print(dataset)
