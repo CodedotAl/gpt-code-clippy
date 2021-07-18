a16fc2b88cdab71618f64459296d0912280ba47b
ncoop57 <nacooper01@email.wm.edu>
2021-07-18 13:49:25 +0000

    Make cli easier to to specify model and clean up code

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -23,23 +23,7 @@ from transformers import (
 # bleu = load_metric("sacrebleu")
 
 MAX_TOKS = 1024
-MAX_NEW_TOKS = 256
-model_name_or_path = "flax-community/gpt-code-clippy-125M-1024-f" # "flax-community/gpt-code-clippy-125M-bs2048-raw" # "EleutherAI/gpt-neo-125M"
-branch = "main"
-
-tokenizer = AutoTokenizer.from_pretrained(
-    model_name_or_path, padding_side="left", pad_token="<|endoftext|>"
-)
-# model = FlaxGPTNeoForCausalLM.from_pretrained(
-#     model_name_or_path,
-#     pad_token_id=50256,
-#     revision=branch
-# )
-model = GPTNeoForCausalLM.from_pretrained(
-    model_name_or_path,
-    pad_token_id=50256,
-    revision=branch
-).to("cuda")
+MAX_NEW_TOKS = 64
 
 
 def generate_text_jax(prompt):
@@ -51,7 +35,7 @@ def generate_text_jax(prompt):
     # print(output)
     return output
 
-def generate_text(prompt, n):
+def generate_text(prompt, n, tokenizer, model):
     inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
     output_seq = model.generate(
         input_ids=inputs.input_ids, max_length=MAX_TOKS,
@@ -87,40 +71,29 @@ def _eval_apps(path):
     eval_and_save_problems(path, path.parent)
 
 
-def _eval_human_eval(path, out_path):
+def _eval_human_eval(path, out_path, tokenizer, model):
     problems = read_problems(str(path))
-    num_samples_per_task = 2
+    num_samples_per_task = 10
     samples = []
     for task_id in tqdm(list(problems.keys())):
-        for text in generate_text(problems[task_id]["prompt"], num_samples_per_task):
+        for text in generate_text(
+                problems[task_id]["prompt"],
+                num_samples_per_task,
+                tokenizer,
+                model
+            ):
             samples.append(dict(task_id=task_id, completion=text))
-    # samples = [
-    #     dict(
-    #         task_id=task_id,
-    #         completion=,
-    #     )
-    #     for text in generate_text(problems[task_id]["prompt"], num_samples_per_task)
-    #     for task_id in tqdm(list(problems.keys()))
-    #     for _ in range(num_samples_per_task)
-    # ]
-    # samples = [
-    #     dict(
-    #         task_id=task_id,
-    #         completion=generate_text(problems[task_id]["prompt"]),
-    #     )
-    #     for task_id in tqdm(list(problems.keys()))
-    #     for _ in range(num_samples_per_task)
-    # ]
     
     write_jsonl(str(out_path / "human_eval.jsonl"), samples)
 
-    # execute bash command to run eval script
-    results = evaluate_functional_correctness(str(out_path / "human_eval.jsonl"), [1, 2], 4, 3.0, str(path))
+    # test out generated functions
+    results = evaluate_functional_correctness(str(out_path / "human_eval.jsonl"), [1, 2, 5, 10], 4, 3.0, str(path))
     print(results)
 
 
 @call_parse
 def main(
+    model_name_or_path: Param("Name or path of model to evaluate", str),
     concode_path: Param("Path to the concode data in CodeXGLUE", str),
     apps_path: Param("Path to the the App dataset", str),
     human_eval_path: Param("Path to the human eval dataset", str),
@@ -133,26 +106,16 @@ def main(
     out_path = out_path / model_name_or_path.split("/")[-1]
     out_path.mkdir(exist_ok=True)
 
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name_or_path, padding_side="left", pad_token="<|endoftext|>"
+    )
+    model = GPTNeoForCausalLM.from_pretrained(
+        model_name_or_path,
+        pad_token_id=50256,
+        # revision=branch
+    ).to("cuda")
+
 
     # _eval_concode(concode_path)
-    _eval_human_eval(human_eval_path, out_path)
+    _eval_human_eval(human_eval_path, out_path, tokenizer, model)
     # _eval_apps(apps_path)
-    # dataset = load_dataset("json", data_files=str(concode_path / "test.json"))
-    # print(dataset)
-    # results = bleu.compute(predictions=predictions, references=references)
-    # print(list(results.keys()))
-    # print(round(results["score"], 1))
-
-
-# problems = read_problems()
-# print(problems)
-# num_samples_per_task = 200
-# samples = [
-#     dict(
-#         task_id=task_id,
-#         completion=generate_text(problems[task_id]["prompt"]),
-#     )
-#     for task_id in problems[:1]
-#     for _ in range(num_samples_per_task)
-# ]
-# write_jsonl("human_eval.jsonl", samples)
